8e82f1d95c7db00728971027a7231174be63671f
Gabriel <gabrielralmeida@gmail.com>
2022-04-02 14:37:08 -0300

    configurando as rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriasModule } from './pages/categorias/categorias.module';
+import { FormCategoriaComponent } from './pages/categorias/form-categoria/form-categoria.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'categorias', loadChildren: () => CategoriasModule },
+  { path: 'formulario', component: FormCategoriaComponent }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
